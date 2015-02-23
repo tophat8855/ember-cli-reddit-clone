d836f6816d3f9b58740fdf704c688030364ec2ff
tophat8855 <hrfarley@gmail.com>
2015-02-22 21:35:39 -0800

    refactored edit and new

--- app/routes/comments/new.js
+++ app/routes/comments/new.js
@@ -3,14 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var post = this.modelFor('posts/show');
-    return this.store.createRecord('comment', {post:post});
+    return this.store.createRecord('comment', {post: post});
   },
-  actions: {
-    createComment: function(model) {
-      var _this = this;
-      model.save().then(function() {
-        _this.transitionTo('comments.index');
-      });
-    }
-  }
+
 });
