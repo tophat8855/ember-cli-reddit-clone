863bfbe52ab516aab431447da254924ffab9c277
tophat8855 <hrfarley@gmail.com>
2015-02-22 20:56:56 -0800

    comments can be created and viewed

--- app/router.js
+++ app/router.js
@@ -7,7 +7,12 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('posts', function() {
-    this.route('show', {path: "/:post_id"});
+    this.route('show', {path: "/:post_id"}, function() {
+      this.resource('comments', function() {
+        this.route('new');
+      });
+    });
+
     this.route('new');
     this.route('edit', {path: '/:post_id/edit'});
   });
